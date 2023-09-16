4e083385ce80f3d0f3c480e1e4c536f0c556c254
thuanpham113 <tphamnguyen@ploggmedia.com>
2023-09-17 01:21:23 +0700

    update notion

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,11 +4,12 @@ module.exports = () => ({
 		config: {
 			endpoint: '/graphql',
 			shadowCRUD: true,
-			playgroundAlways: false,
+			playgroundAlways: true,
 			depthLimit: 7,
 			amountLimit: 100,
 			apolloServer: {
 				tracing: false,
+				introspection: true
 			},
 		},
 	},
